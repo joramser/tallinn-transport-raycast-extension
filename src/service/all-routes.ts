2f775a4e6c0f5cbc0b0056259e16370a26bb933e
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2025-08-30 18:47:40 +0300

    Implement caching for endpoints to reduce request number

--- src/service/all-routes.ts
+++ src/service/all-routes.ts
@@ -1,9 +1,28 @@
 import { fetchRoutes, fetchStops, type RouteRaw, type StopRaw } from "@/api";
-import { extractAllRoutes } from "@/lib/routes";
-import { extractAllStops } from "@/lib/stops";
+import { extractAllRoutes, type Route } from "@/lib/routes";
+import { extractAllStops, type Stop } from "@/lib/stops";
 import Papa from "papaparse";
+import { Cache } from "@raycast/api";
+
+const cache = new Cache();
+
+const ROUTES_CACHE_TIMESTAMP_KEY = "ROUTES_CACHE_TIMESTAMP";
+const CACHE_EXPIRY = 1000 * 60 * 30;
+const ROUTES_CACHE_KEY = "ROUTES_DATA";
+const STOPS_CACHE_KEY = "STOPS_DATA";
 
 export const getAllRoutesData = async () => {
+  if (cache.get(ROUTES_CACHE_TIMESTAMP_KEY)) {
+    const cachedTimestamp = parseInt(cache.get(ROUTES_CACHE_TIMESTAMP_KEY) || "0", 10);
+    const currentTime = Date.now();
+
+    if (currentTime - cachedTimestamp < CACHE_EXPIRY) {
+      const routes = JSON.parse(cache.get(ROUTES_CACHE_KEY) || "[]") as Route[];
+      const stops = new Map<string, Stop>(JSON.parse(cache.get(STOPS_CACHE_KEY) || "[]"));
+      return { routes, stops };
+    }
+  }
+
   const [routesRaw, stopsRaw] = await Promise.all([fetchRoutes(), fetchStops()]);
 
   const parsedRoutes = Papa.parse<RouteRaw>(routesRaw, { header: true, delimiter: ";" });
@@ -18,6 +37,10 @@ export const getAllRoutesData = async () => {
 
   const { routes, relevantStopIds } = extractAllRoutes(parsedRoutes.data);
   const { stops } = extractAllStops(parsedStops.data, relevantStopIds);
+
+  cache.set(ROUTES_CACHE_KEY, JSON.stringify(routes));
+  cache.set(STOPS_CACHE_KEY, JSON.stringify(Array.from(stops)));
+  cache.set(ROUTES_CACHE_TIMESTAMP_KEY, Date.now().toString());
 
   return { routes, stops };
 };
--- src/service/announcements.ts
+++ src/service/announcements.ts
@@ -1,5 +1,27 @@
-import { fetchAnnouncements, type AnnouncementRaw } from "@/api";
+import { fetchAnnouncements } from "@/api";
+import { Cache } from "@raycast/api";
 
-export const getAnnouncements = () => {
-  return fetchAnnouncements() as Promise<AnnouncementRaw[]>;
+const cache = new Cache();
+
+const ANNOUNCEMENTS_CACHE_KEY = "ANNOUNCEMENTS_DATA";
+const ANNOUNCEMENTS_CACHE_TIMESTAMP_KEY = "ANNOUNCEMENTS_CACHE_TIMESTAMP";
+
+export const getAnnouncements = async () => {
+  if (cache.get(ANNOUNCEMENTS_CACHE_TIMESTAMP_KEY)) {
+    const cachedTimestamp = parseInt(cache.get(ANNOUNCEMENTS_CACHE_TIMESTAMP_KEY) || "0", 10);
+    const currentTime = Date.now();
+    const cacheDuration = 1000 * 60 * 30;
+
+    if (currentTime - cachedTimestamp < cacheDuration) {
+      const announcements = JSON.parse(cache.get(ANNOUNCEMENTS_CACHE_KEY) || "[]");
+      return Promise.resolve(announcements);
+    }
+  }
+
+  const data = await fetchAnnouncements();
+
+  cache.set(ANNOUNCEMENTS_CACHE_KEY, JSON.stringify(data));
+  cache.set(ANNOUNCEMENTS_CACHE_TIMESTAMP_KEY, Date.now().toString());
+
+  return data;
 };
